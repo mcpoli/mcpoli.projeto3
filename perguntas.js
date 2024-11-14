a7ba7501b6d70b649a0ab372d3a9de43be000723
mcpoli <00001080960739sp@al.educacao.sp.gov.br>
2024-11-14 13:43:39 -0300

    Update and rename README.md to perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'princesas',
+    'qual a princesa da Disney conhecida por ter muito sono?',
+    ' A Bela Adormecida'
+)
+
+criaCartao(
+    'Turismo',
+    'Qual cidade é conhecida como ciadade das flores?',
+    'A cidade é Holambra'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua espanhola',
+    'Como se diz oi em Espanhol?',
+    'Oi em Espanhol é HOLA (OLA)'
+)
